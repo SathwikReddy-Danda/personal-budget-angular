492752085d509dd802bae10b3497b0f1052ad8af
Sathwik Reddy Danda <sathwikreddydanda@Sathwiks-Air.lan>
2023-09-23 13:50:54 -0400

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
+
+//app.use('/', express.static('public'));
 
 
 const budget = require('./myfile.json');
